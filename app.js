aa22185c3e1294d054679acb92855a7af3de0b9d
Your Name <you@example.com>
2026-03-03 11:04:13 +0500

    Updated new changes

--- app.js
+++ app.js
@@ -55,14 +55,33 @@ button.addEventListener("click", function(){
         todos.push(task);
         renderTodos();
         input.value = "";
-    
 
-    function renderTodos() {
+
+   function renderTodos() {
         list.innerHTML = "";
-        todos.forEach(function (todo){
+
+        todos.forEach(function (todo, index){
             let li = document.createElement("li");
             li.innerText = todo;
+            let deleteBtn = document.createElement("button");
+            deleteBtn.innerText = "Delete";
+            deleteBtn.style.margin = "20px";
+            deleteBtn.style.padding = "10px 10px";
+            deleteBtn.style.backgroundColor = "red";
+            deleteBtn.style.color = "white";
+            deleteBtn.style.borderRadius = "5px";
+            deleteBtn.style.border = "none";
+            deleteBtn.style.fontSize = "18px";
+
+            deleteBtn.addEventListener("click", function(){
+                todos.splice(index, 1);
+                renderTodos();
+            })
+
+            li.appendChild(deleteBtn);
+
             list.appendChild(li);
         })
-    }
+   }
+
 })
